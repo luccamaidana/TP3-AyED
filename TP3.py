9ab92f4bdc4356dabc73fa786fed77d890b2d6e0
PROgrammer <pennicegaston@gmail.com>
2023-09-22 01:28:30 -0300

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -12,6 +12,13 @@ def clear_screen():
     else:
         os.system('clear')
 
+import colorama
+from colorama import Fore, Style
+
+# Inicializar colorama
+colorama.init(autoreset=True)
+
+
 #--------------------------CLASS------------------------------
 class user:
     def __init__(self):
@@ -56,34 +63,40 @@ class novedades:
 
 #------------------------PP-----------------------------------
 #user
-afu = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USUARIOS.dat"
+#afu = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USUARIOS.dat"
+afu = "c:\\Users\\Gaston\\Documents\\GitHub\\TP2-AyED\\TP3-AyED\\USUARIOS.dat"
 alu = open (afu, "w+b")
 regUser = user()
 
 #locales
-afl = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\LOCALES.dat"
+#afl = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\LOCALES.dat"
+afl = "c:\\Users\\Gaston\\Documents\\GitHub\\TP2-AyED\\TP3-AyED\\LOCALES.dat"
 #all = open (afl, "w+b")
 regLoc = locales()
 
 #promos
-afp = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\PROMOCIONES.DAT"
+#afp = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\PROMOCIONES.DAT"
+afp = "c:\\Users\\Gaston\\Documents\\GitHub\\TP2-AyED\\TP3-AyED\\PROMOCIONES.DAT"
 #alp = open (afp, "w+b")
 regProm = promociones()
 
 #uso promos
-afup = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USO_PROMOCIONES.DAT"
+#afup = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USO_PROMOCIONES.DAT"
+afup = "c:\\Users\\Gaston\\Documents\\GitHub\\TP2-AyED\\TP3-AyED\\USO_PROMOCIONES.DAT"
 #alup = open (afup, "w+b")
 regUP = uso_promociones()
 
 #novedades
-afn = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\NOVEDADES.DAT"
+#afn = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\NOVEDADES.DAT"
+afn = "c:\\Users\\Gaston\\Documents\\GitHub\\TP2-AyED\\TP3-AyED\\NOVEDADES.DAT"
 #aln = open (afn, "w+b")
 regNov = novedades()
 
 #-------------------------CARGAS---------------------------
+
 regUser.cod=1
-regUser.usuario="admin@shopping.com"
-regUser.clave="12345"
+regUser.usuario="5"
+regUser.clave="6"
 regUser.tipo="administrador"
 pickle.dump(regUser,alu)
 
@@ -128,20 +141,77 @@ def mostrar_menu():
     print("1_ Ingresar con usuario registrado\n2_ Registrarse como cliente\n3_ Salir")
 
 
+#-------------------------LOGIN----------------------------
+
+def login():
+    import maskpass
+    alu = open (afu, "r+b")
+    size=os.path.getsize(afu)
+    correcto=0
+    cont=1
+    nombre=input("\nIngrese el nombre: ")
+    password = maskpass.askpass(prompt="\nIngresar contraseña: ", mask="*")
+    #registro=pickle.load(alu)
+    #1
+    while correcto!=1 and cont<3:
+        alu.seek(0,0)
+        while(alu.tell() < size) and (regUser.usuario!=nombre) and (regUser.clave!=password):
+            registro=pickle.load(alu)
+
+        if (regUser.usuario==nombre and regUser.clave==password):  
+            correcto=1
+        else:
+            nombre=input("\nIngrese el nombre: ")
+            password = maskpass.askpass(prompt="\nIngresar contraseña: ", mask="*")
+            cont=cont+1
+            alu.seek(0,0)
+            print(cont)
+    if(correcto==1):
+        god=regUser.tipo
+        #selec_menu(regUser.tipo)
+    else:
+        print("Cantidad de maxima de intentos permitidos")
+
+#------------------------VALIDADORES-------------------------
+
+def valid_opc():
+    global opc
+    opc = input("\nOPCION: ")
+    clear_screen()
+    while opc != "1" and opc != "2" and opc != "3":
+        mostrar_menu()
+        opc = input("\nMal ingresado. Repetir opción. OPCION: ")
+        clear_screen()
+    clear_screen()
+    return opc
+
+#CASE
+def ingreso_datos(k):
+    match k:
+        case "1":
+            login()
+        case "2":
+            print("\nSaliendo...")
+            #signin()
+        case "3":
+            print("\nSaliendo...")
+
+
 #-------------------------MENU'S----------------------------
 #Main Menu
 def mainMenu():
     clear_screen()
     mostrar_menu()
-
-    opc= input("Ingrese la opcion: ")
-
+    ingreso_datos(valid_opc())
 
 
 def PP():
     mainMenu()
 
 PP()
+
+
+
 
 
 
