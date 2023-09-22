0b1982f602aa1d9f7bc9b69296642ccddb8f2d0b
luccamaidana <luccamaidana03@gmail.com>
2023-09-21 23:26:21 -0300

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -1,10 +1,18 @@
-#import
+#------------------------IMPORT-------------------------------
 import os
 import pickle
 import os.path
 import io
+import shutil
+def clear_screen():
+    # Comando para limpiar la pantalla en Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Comando para limpiar la pantalla en sistemas basados en Unix (Linux, macOS, etc.)
+    else:
+        os.system('clear')
 
-#class
+#--------------------------CLASS------------------------------
 class user:
     def __init__(self):
         self.cod = 0
@@ -46,8 +54,7 @@ class novedades:
         self.tipoUsuario = ""
         self.estado = ""
 
-
-#PP
+#------------------------PP-----------------------------------
 #user
 afu = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USUARIOS.dat"
 alu = open (afu, "w+b")
@@ -73,10 +80,7 @@ afn = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\NOVEDADES.DAT"
 #aln = open (afn, "w+b")
 regNov = novedades()
 
-
-
-
-
+#-------------------------CARGAS---------------------------
 regUser.cod=1
 regUser.usuario="admin@shopping.com"
 regUser.clave="12345"
@@ -95,7 +99,7 @@ pickle.dump(regUser, alu)
 alu.flush()
 alu.close()
 
-#lectura
+#------------------------LECTURAS-------------------------
 alu = open (afu,"r+b")
 tamU= os.path.getsize(afu)
 
@@ -107,3 +111,37 @@ while alu.tell() < tamU:
     print(regUser.tipo)
 
 alu.close()
+
+
+#------------------------PANTALLAS-------------------------
+def mostrar_menu():
+    ancho_ventana = shutil.get_terminal_size().columns
+
+    menu_texto = "Menú"
+
+    espacio_blancos = (ancho_ventana - len(menu_texto) - 2) // 2
+
+    print("-" * ancho_ventana)
+    print(f"|{' ' * espacio_blancos}{menu_texto}{' ' * espacio_blancos}|")
+    print("-" * ancho_ventana)
+    print("\nIngrese una opcion 1-3\n")
+    print("1_ Ingresar con usuario registrado\n2_ Registrarse como cliente\n3_ Salir")
+
+
+#-------------------------MENU'S----------------------------
+#Main Menu
+def mainMenu():
+    clear_screen()
+    mostrar_menu()
+
+    opc= input("Ingrese la opcion: ")
+
+
+
+def PP():
+    mainMenu()
+
+PP()
+
+
+
