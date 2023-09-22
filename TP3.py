512cce7e6c5c5a84ccd4ce0d7068c01ac26265fe
luccamaidana <luccamaidana03@gmail.com>
2023-09-21 21:09:05 -0300

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -5,15 +5,45 @@ import os.path
 import io
 
 #class
-class USUARIOS:
+class user:
     def __init__(self):
         self.cod = 0
-        self.user = ""
+        self.usuario = ""
         self.clave = ""
         self.tipo = ""
 
 #PP
-os.makedirs("users")
-afu = "C:\Users\lucca\Desktop\UTN\AyED\TP\TP3-AyED\USUARIOS.dat"
+afu = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USUARIOS.dat"
 alu = open (afu, "w+b")
-regUser = USUARIOS()
+regUser = user()
+
+regUser.cod=1
+regUser.usuario="admin@shopping.com"
+regUser.clave="12345"
+regUser.tipo="administrador"
+pickle.dump(regUser,alu)
+
+regUser.usuario = regUser.usuario.ljust(100)
+regUser.clave = regUser.clave.ljust(8)
+regUser.tipo = "usuario"
+
+regUser.cod=2
+regUser.usuario= input("\nUsuario:")
+regUser.clave= input("\nClave:")
+pickle.dump(regUser, alu)
+
+alu.flush()
+alu.close()
+
+#lectura
+alu = open (afu,"r+b")
+tamU= os.path.getsize(afu)
+
+while alu.tell() < tamU:
+    regUser = pickle.load(alu)
+    print(regUser.cod)
+    print(regUser.usuario)
+    print(regUser.clave)
+    print(regUser.tipo)
+
+alu.close()
