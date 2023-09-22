1347abd019ff998c0a6349fcd65067317c88e9ad
luccamaidana <luccamaidana03@gmail.com>
2023-09-21 21:34:21 -0300

    Update TP3.py

--- TP3.py
+++ TP3.py
@@ -12,10 +12,70 @@ class user:
         self.clave = ""
         self.tipo = ""
 
+class locales:
+    def __init__(self):
+        self.codLocal = 0
+        self.nombreLocal = ""
+        self.ubicacionLocal = ""
+        self.rubroLocal = ""
+        self.codUsuario = ""
+        self.estado = ""
+
+class promociones:
+    def __init__(self):
+        self.codPromo = 0
+        self.textoPromo = ""
+        self.fechaDesdePromo = ""
+        self.fechaHastaPromo = ""
+        self.diasSemana = ""
+        self.estado = ""
+        self.codLocal = 0
+
+class uso_promociones: 
+    def __init__(self):
+        self.codCliente = 0
+        self.codPromo = ""
+        self.fechaUsoPromo = ""
+
+class novedades:
+    def __init__(self):
+        self.codNovedad = 0
+        self.textoNovedad = ""
+        self.fechaDesdeNovedad = ""
+        self.fechaHastaNovedad = ""
+        self.tipoUsuario = ""
+        self.estado = ""
+
+
 #PP
+#user
 afu = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USUARIOS.dat"
 alu = open (afu, "w+b")
 regUser = user()
+
+#locales
+afl = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\LOCALES.dat"
+#all = open (afl, "w+b")
+regLoc = locales()
+
+#promos
+afp = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\PROMOCIONES.DAT"
+#alp = open (afp, "w+b")
+regProm = promociones()
+
+#uso promos
+afup = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\USO_PROMOCIONES.DAT"
+#alup = open (afup, "w+b")
+regUP = uso_promociones()
+
+#novedades
+afn = "c:\\Users\\lucca\\Desktop\\UTN\\AyED\\TP\\TP3-AyED\\NOVEDADES.DAT"
+#aln = open (afn, "w+b")
+regNov = novedades()
+
+
+
+
 
 regUser.cod=1
 regUser.usuario="admin@shopping.com"
